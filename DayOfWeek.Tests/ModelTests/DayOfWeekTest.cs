1dda348e42fdc6d05d2c411065908a80c62a7286
Michael Albers and Nico Daunt <michael@>
2018-02-15 15:05:02 -0800

    create red test condition for GetDay test method

--- DayOfWeek.Tests/ModelTests/DayOfWeekTest.cs
+++ DayOfWeek.Tests/ModelTests/DayOfWeekTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DayOfWeek;
+using DayOfWeek.Models;
+using System;
+using System.Collections.Generic;
 
 namespace DayOfWeek.Tests
 {
@@ -7,9 +9,16 @@ namespace DayOfWeek.Tests
   public class DayOfWeekTest
   {
     [TestMethod]
-    public void GetDate_ReturnDate()
+    public void GetDay_ReturnDay()
     {
+        //arrange
+        List<int> testDay = new List<int> {2018, 02, 15};
+        DayOfWeekGenerator newDWG = new DayOfWeekGenerator(2018, 02, 15);
 
+        //act
+        List<int> dateInput = new List<int> {newDWG.GetYear(), newDWG.GetMonth(), newDWG.GetDay()};
+        //assert
+        CollectionAssert.AreEqual(testDay, dateInput);
     }
   }
 }
--- DayOfWeek/Models/DayOfWeek.cs
+++ DayOfWeek/Models/DayOfWeek.cs
@@ -2,9 +2,35 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 
-namespace DayOfWeek
+namespace DayOfWeek.Models
 {
   public class DayOfWeekGenerator
   {
+      private int _day;
+      private int _month;
+      private int _year;
+
+      public DayOfWeekGenerator(int day, int month, int year)
+      {
+          _day = day;
+          _month = month;
+          _year = year;
+      }
+
+      public int GetYear()
+      {
+          return 5;
+      }
+
+      public int GetDay()
+      {
+          return 0;
+      }
+
+      public int GetMonth()
+      {
+          return 0;
+      }
+
   }
 }
